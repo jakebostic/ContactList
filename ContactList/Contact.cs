4bbd3b1a5956d2046c0b4a1059ee3c3a97436910
jakebostic <jake_bostic@yahoo.com>
2018-12-14 14:01:48 -0500

    Initial Commit

---
+++ ContactList/Contact.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactList
+{
+    class Contact
+    {
+        private String fname;
+        private String lname;
+        private String email;
+        private String phone;
+
+        public Contact()
+        {
+
+        }
+
+        public Contact(String fname, String lname, String email, String phone)
+        {
+            this.fname = fname;
+            this.lname = lname;
+            this.email = email;
+            this.phone = phone;
+        }
+
+        public void setFirstName(String fname)
+        {
+            this.fname = fname;
+        }
+        public String getFirstName()
+        {
+            return fname;
+        }
+
+        public void setLastName(String lname)
+        {
+            this.lname = lname;
+        }
+        public String getLastName()
+        {
+            return lname;
+        }
+
+        public void setEmail(String email)
+        {
+            this.email = email;
+        }
+        public String getEmail()
+        {
+            return email;
+        }
+
+        public void setPhone(String phone)
+        {
+            this.phone = phone;
+        }
+        public String getPhone()
+        {
+            return phone;
+        }
+
+        public String displayContact()
+        {
+            string contact = "--------------------------------------------\n";
+            contact += "---- Current Contact -----------------------\n";
+            contact += "--------------------------------------------\n";
+            contact += "Name:          " + fname + " " + lname + "\n";
+            contact += "Email Address: " + email + "\n";
+            contact += "Phone Number:  " + phone + "\n";
+            contact += "--------------------------------------------\n";
+            return contact;
+        }
+    }
+}
---
+++ ContactList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ContactList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Contact List Application");
+            Console.WriteLine();
+
+            Contact contact = new Contact();
+
+            String fname = getString("Enter first name: ");
+            contact.setFirstName(fname);
+
+            String lname = getString("Enter last name: ");
+            contact.setLastName(lname);
+
+            String email = getString("Enter email: ");
+            contact.setEmail(email);
+
+            String phone = getString("Enter phone: ");
+            contact.setPhone(phone);
+            Console.WriteLine();
+
+            Console.Write(contact.displayContact());
+            Console.WriteLine();
+
+            Console.Write("Press any key to exit...");
+            Console.ReadKey(true);
+
+        }
+
+        private static String getString(String prompt)
+        {
+            Console.Write(prompt);
+            String s = Console.ReadLine();
+            return s;
+        }
+    }
+
+}
